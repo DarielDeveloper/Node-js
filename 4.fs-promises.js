97163d576fbdccbba75a8d0d4d606117394272cf
darielna <darielna910806@gmail.com>
2024-04-08 00:47:05 -0400

    Ejemplo de ejecución de fs con promises

---
+++ 4.fs-promises.js
@@ -0,0 +1,14 @@
+//Esto solo en los módulos nativos que no tienen promesas nativas
+
+//const {promisify}=require{'node:util'}
+//const readFilePromise= promisify(fs.readFile)
+
+const fs = require('node:fs/promises')
+
+console.log('Leyendo el primer archivo');
+fs.readFile('./archivo.txt', 'utf-8')
+  .then(text => {
+    console.log('primer texto: ' + text);
+  })
+
+console.log('Hacer cosas mientras lee el archivo');
